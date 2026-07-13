b885224bb5571dbaa2c01ee36515c7c78ad2a687
bruxabranca <138905524+bruxabranca@users.noreply.github.com>
2026-07-12 21:55:20 -0300

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,26 @@
+﻿{
+	"version": 1783904040,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-3.4.1.min.js",
+		"offlineClient.js",
+		"images/imagespawn-sheet0.png",
+		"images/imagespawn-sheet1.png",
+		"images/imagespawn-sheet2.png",
+		"images/imagespawn-sheet3.png",
+		"images/imagespawn-sheet4.png",
+		"images/imagespawn-sheet5.png",
+		"images/imagespawn-sheet6.png",
+		"images/imagespawn-sheet7.png",
+		"images/onlineball-sheet0.png",
+		"images/onlineball-sheet1.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png",
+		"adapter.js"
+	]
+}
